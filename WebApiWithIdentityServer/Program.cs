c1a751573a72bf10c763917f4c25f5ebfabfc0dd
Sojib Khan <mdsojibkhan.007@gmail.com>
2024-12-18 23:31:39 +0600

    Added-authorize-option-in-swagger

--- WebApiWithIdentityServer/Program.cs
+++ WebApiWithIdentityServer/Program.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.OpenApi.Models;
 using WebApiWithIdentityServer.Configurations;
 
 namespace WebApiWithIdentityServer
@@ -28,7 +29,39 @@ namespace WebApiWithIdentityServer
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+
+                // Add Security Definition for Bearer token
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                // Add Security Requirement
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
 
             var app = builder.Build();
 
